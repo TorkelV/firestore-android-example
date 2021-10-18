6e36f396b306b13e0c8499714547da13a6f32483
TorkelV <torkel93@gmail.com>
2021-10-18 15:39:16 +0200

    init

---
+++ app/src/main/java/no/lekrot/firestore_android_example/MainActivity.kt
@@ -0,0 +1,11 @@
+package no.lekrot.firestore_android_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
